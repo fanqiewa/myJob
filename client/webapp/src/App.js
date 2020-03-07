2b5f3084b4a4659f0a022a40568201737332c71c
ze <15978243320@163.com>
2020-03-07 21:42:47 +0800

    !

--- client/webapp/src/App.js
+++ client/webapp/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Product = () => (
+  <div>
+    <h2>Product</h2>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Product">Product</Link>
+        <hr/>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/product" component={Product}></Route>
+      </div>
+    </Router>
   );
 }
 
